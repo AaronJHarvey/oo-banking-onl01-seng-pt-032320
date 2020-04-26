03a237054dcf9d6d6ecb8275a4bba448227ca42b
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-26 20:59:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,6 +16,7 @@ class Transfer
  def execute_transaction
    if valid? && sender.balance > amount && self.status == "pending"
      sender.balance -= amount
+     receiver.balance += amount 
    end
  end
  
